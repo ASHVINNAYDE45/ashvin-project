70829a3d2dfb2335ce3551f12eaf32ae8214a7af
ashvin <ashvinnayde@gmail.com>
2023-12-26 16:23:02 +0530

    add file

--- components/landingPage/index.js
+++ components/landingPage/index.js
@@ -3,11 +3,15 @@ import NavbarPage from '../layout/navbarPage';
 import CounterSection from './counterSection';
 import CounterSections from './counterSections';
 import TopSection from './topSection';
+import BecomeAnInstructor from './becomeAnInstructor'
+
 function LandingPage() {
   return (
   <main>
   <TopSection/>
   <CounterSections/>
+  <BecomeAnInstructor/>
+ 
   </main>
   )
 }
